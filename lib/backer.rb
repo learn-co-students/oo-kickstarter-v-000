0230bf99f086cef6ea8f77109a894c977514be1f
jenjiyi <jenny.yamada@gmail.com>
2016-01-25 12:35:08 -0800

    clean up

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,5 @@
 class Backer
-  attr_accessor :backed_projects, :name, :title
-
+  attr_accessor :backed_projects, :name
   def initialize(name)
     @name = name
       @backed_projects = []
--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,5 @@
 class Project
-  attr_accessor :backers, :title, :name
+  attr_accessor :backers, :title
   
   def initialize(title)
     @title = title
