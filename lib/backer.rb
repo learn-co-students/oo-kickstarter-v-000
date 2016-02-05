ea3ca94a24ac9ee1e81226da3a95857472c2f27a
glee38 <glee3388@gmail.com>
2016-02-05 12:20:23 -0500

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,6 +8,7 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
+    #because "project" is really equivalent to an instance of Project, or Project.new("name_of_project"), you can call the .backers method on it, which in this case, returns an array. then, self (aka this instance of backer) is shoveled into the backers array. 
     project.backers << self
   end
 end
