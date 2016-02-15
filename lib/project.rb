96659aeacd1cec7c579750891f8963d35aa72966
shinyrachel <rachel.kory@gmail.com>
2016-02-15 10:51:10 -0500

    complete

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,15 @@
 class Project
 
-  attr_accessor :title
+  attr_accessor :title, :backers
 
   def initialize(title)
     @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
   end
 
 end
