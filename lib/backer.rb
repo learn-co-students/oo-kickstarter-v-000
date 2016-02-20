be93fd8aba5bef80f77163a36de03dfff4608ff4
rocketNeck <homestarflyer@gmail.com>
2016-02-20 15:59:57 -0500

    that helped for real

---
+++ lib/backer.rb
@@ -0,0 +1,24 @@
+class Backer
+  attr_accessor :backed_projects, :name
+
+  def initialize(name)
+    @name = name
+    self.backed_projects = []
+  end
+
+  def back_project(project)
+    self.backed_projects << project
+    project.backers << self
+  end
+
+
+
+
+
+
+
+
+
+
+
+end
