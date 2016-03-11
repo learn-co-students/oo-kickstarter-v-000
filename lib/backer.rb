12436b73e64f2aad7125b612f2e5f079a85e5a1d
Kenneth Amper <kenn.amp@gmail.com>
2016-03-11 04:37:09 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Backer
+
+  attr_accessor :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    if !backed_projects.include?(project)
+      backed_projects << project
+      project.add_backer(self)
+    end
+    #binding.pry
+  end
+
+
+
+end
