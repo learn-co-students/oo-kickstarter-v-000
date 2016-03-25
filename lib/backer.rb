04fda85218d24e2072d7b9d80dff46df69c6d419
Rami97 <rami-sebastian@web.de>
2016-03-25 00:06:09 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,4 +4,14 @@ attr_accessor :name
     @name = name
     @backed_projects = []
   end
+
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+
+  def backed_projects
+    @backed_projects
+  end
+
 end
