27e3f2a20912b05430c232f02117c24e70c038e7
hangloos <ericloos00@gmail.com>
2016-04-19 13:27:14 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Project
+
+attr_accessor :backers, :title
+
+def initialize(title)
+  @title = title
+  @backers = []
+end
+
+def add_backer(backer)
+  @backers << backer
+  
+end
+
+
+
+end
