64861ce20ab660fcd37e7d07e34153f925249fad
Michael Randell <michael.james.randell@gmail.com>
2016-04-28 10:52:13 -0400

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Backer
+  attr_accessor :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  #def backed_projects
+  #  @backed_projects
+  #end
+
+  def back_project(project)
+    @backed_projects << project
+    it = Project.new(project)
+    it.backers << self
+  end
+
+
+
+
+
+end
