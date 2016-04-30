6e3f3d318a6e6c7f4b741578239000601c2c538a
mjtavarez <mjtavarez@yahoo.com>
2016-04-30 12:57:52 -0400

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+class Backer
+  attr_accessor :backed_projects, :back_project
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    self.backed_projects << project
+    project.backers << self
+  end
+
+  def backed_projects
+    @backed_projects
+  end
+
+end
