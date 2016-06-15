838c05dec64d56ac10f307a9a413e1bd781a3716
andrewstone8 <andrewstone8@gmail.com>
2016-06-15 13:30:45 +0900

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,20 @@
+class Project
+
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def backers
+    @backers
+  end
+
+  def add_backer(backer)
+    self.backers << backer
+    backer.backed_projects << self
+  end
+
+
+end # <-- end class -->
