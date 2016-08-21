f2ba6e740715c4ba8d97d2242bda45f9d8abd0c5
Christina Cole <christinal.cole@gmail.com>
2016-08-21 23:09:05 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project
+
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer_name)
+    @backers << backer_name
+    backer_name.backed_projects << self
+  end
+end
