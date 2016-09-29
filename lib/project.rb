bc11b455085055f0f4c7e3baa5d29c2823c28842
Aya Fushimi <aya.fushimi@gmail.com>
2016-09-29 21:38:39 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+class Project
+  attr_accessor :backers
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer_instance)
+    self.backers << backer_instance
+    backer_instance.backed_projects << self
+  end
+end
