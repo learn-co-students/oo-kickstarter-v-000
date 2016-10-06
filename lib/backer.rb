b23173c42054fa3faee71d343748e0b492a65533
vanita eckert <vanita112003@gmail.com>
2016-10-06 00:19:05 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,24 @@
+require "pry"
+class Backer
+
+attr_reader :name
+attr_accessor :backed_projects
+
+
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+
+  def back_project(project)
+    self.backed_projects << project
+    project.backers << self
+    # raise @backed_projects.inspect
+  end
+
+
+
+
+end
