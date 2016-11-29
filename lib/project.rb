ec7959276671bca8c6fbd6632c9ee83d99fb1b97
Callie Brazil <cmabrazil@gmail.com>
2016-11-29 20:10:28 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,21 @@
+class Project
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def title
+    @title
+  end
+
+  def add_backer(name)
+    @backers << name
+    name.backed_projects << self
+  end
+
+  def backers
+    @backers
+  end
+end
