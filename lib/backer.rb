31df7e02fd217a8c2da5cf8018fad593a5d573be
Scott Wakeman <swakeman@ca.rr.com>
2017-01-23 20:24:00 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,14 @@
+class Backer
+  attr_accessor :name, :backed_projects, :add_backer
+    @@backers=[]
+    def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+end
+
