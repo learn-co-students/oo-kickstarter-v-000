37a622a145380bd785b9351f2dfbc07d05344df9
John Espinosa <johnfelix.espinosa@icloud.com>
2017-01-31 01:01:27 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,17 @@
 class Backer
 
-	attr_accessor :name, :backed_projects
+  attr_accessor :name, :backed_projects
 
-	def initialize(name)
-		@name = name
-		@backed_projects = []
-	end
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
 
-	def back_project(project)
-		backed_projects << project
-		project.backers << self
-	end
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+
+
 
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,15 +1,15 @@
 class Project
 
-	attr_accessor :title, :backers
+  attr_accessor :title, :backers
 
-	def initialize(title)
-		@title = title
-		@backers = []
-	end
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
 
-	def add_backer(backer)
-		@backers << backer
-		backer.backed_projects << self
-	end
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
 
 end
