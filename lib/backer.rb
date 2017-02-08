8bf094e1b567bf3be1e16e9d2ee3811912b971b7
Parv Kumar <pkumar@trinity.edu>
2017-02-08 20:08:26 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+class Backer
+
+  attr_accessor :backed_projects, :name
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    unless project.backers.include?(self)
+      project.add_backer(self)
+    end
+  end
+
+
+end
