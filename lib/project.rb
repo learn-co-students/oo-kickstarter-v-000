2f908ff7bfacb0baa9f23fa3b14dd612a6dcc06c
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-02-13 23:15:05 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,20 @@
+class Project
+ attr_accessor :title
+ attr_reader :backers
+
+ def initialize(title)
+   @title = title
+   @backers = []
+ end
+
+ def backers
+   @backers.dup.freeze
+ end
+
+ def add_backer(backer)
+   @backers << backer
+   backer.back_project(self) unless backer.backed_projects.include?(self)
+
+ end
+
+end
