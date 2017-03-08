2093ea77282518390a1b4b4a39f991390ad7c403
Kelly Kashuda <klkashuda@crimson.ua.edu>
2017-03-08 22:17:47 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    project = self
+    backer.backed_projects << project
+  end
+
+
+
+end
