6cab2396c5b3fd0aab3efa906f8373d747582f58
Neil Ryan <neilmarkoliverryan@gmail.com>
2017-03-21 02:05:49 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project
+
+  attr_accessor :backers
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer_instance)
+    @backers << backer_instance
+    backer_instance.backed_projects << self
+  end
+
+end
