9b56ae628b301543ead5053832b47bc939bbb8db
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-04-12 05:00:39 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+class Project
+attr_accessor :title
+
+def initialize(title)
+  @title = title
+  @backers = []
+end
+
+def add_backer(backer)
+    @backers << backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
+end
+
+def backers
+  @backers
+end
+
+end
