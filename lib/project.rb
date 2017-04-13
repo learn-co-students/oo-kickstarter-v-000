cc85e62a8e90ec6abae5b2c84c6a25fcdc493f42
Matteo Piccini <gnappo.1@live.it>
2017-04-13 21:17:55 +0000

    Done.

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,6 @@ class Project
 
   def add_backer(backer)
     @backers << backer
-    backer.back_project(self) unless backer.back_project.include?(self)
+    backer.back_project(self) unless backer.backed_projects.include?(self)
   end
 end
