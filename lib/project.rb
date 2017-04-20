e994d137bc643b9e4606d1e212aaa03f0f0e0063
Zain <zain@halai.us>
2017-04-19 22:22:05 -0500

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project
+	attr_reader :title, :backers
+
+	def initialize(title)
+		@title = title
+		@backers = []
+	end
+
+	def add_backer(baker)
+		backers << baker
+		baker.backed_projects << self
+	end
+end
