4e799c65f93aafe8c8d8eec12b8e1ade46a668de
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-05-10 16:04:48 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,24 @@
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self 
+  end
+
+  def backers
+     @backers
+  end
+
+  def self.Backer
+    @backers << Backer.new.name
+    
+  end
+
+  
+end
