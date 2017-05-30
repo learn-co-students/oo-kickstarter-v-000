b80ef9d49c8ecce3793c2315c8ef0f7c7453c319
Derek  Pelt <derekpelt@gmail.com>
2017-05-30 07:47:40 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,8 @@ class Backer
 
   def back_project(project)
     @backed_projects  << project
-    project.add_backer(self) unless project.backers.include?(self)
+    project.backers << self 
+    #project.add_backer(self) unless project.backers.include?(self)
   end
 
 
