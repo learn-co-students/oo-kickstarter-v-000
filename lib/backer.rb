808f67a3c280f3ee60ee0326541d69f485830297
Sam Marshall <samuel.r.marshall@gmail.com>
2017-06-05 01:15:07 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+class Backer
+
+  attr_accessor :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    if !@backed_projects.detect{|projects| projects == project}
+      @backed_projects << project
+      project.add_backer(self)
+    end
+  end
+
+
+
+end
