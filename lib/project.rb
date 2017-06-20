ea0185e1236ec657ef5809fb12f7d5fdda8b8855
Brian Lane <mr.brianlane@gmail.com>
2017-06-20 17:05:33 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+class Project
+
+  attr_accessor :backers, :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  #instance methods
+
+  def add_backer(backer)
+    self.backers << backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
+  end
+
+end
