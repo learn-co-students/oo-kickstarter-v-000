f80dc735fc0dfdae0aa88da05efc015e29563da6
Dominique De Leon <dominique.a.deleon@gmail.com>
2017-08-07 23:14:04 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer
+  attr_reader :name
+  attr_accessor :backed_projects, :backer
+
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(title)
+    @backed_projects << title
+    
+  end
+
+end
