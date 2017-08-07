afdf6eb0acce24e349a309d67484cded07a88fa2
Cody Kasselman <ckasselman@fordham.edu>
2017-08-07 19:26:07 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+class Backer
+
+attr_accessor :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def name
+    @name
+  end
+
+  def back_project(project)
+    self.backed_projects << project
+    project.backers << self
+  end
+
+end
