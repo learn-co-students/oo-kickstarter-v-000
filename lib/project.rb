22839fe28071425a31bd1c411e39092c5b8b15c6
Jim Stricker <jmstricker93@gmail.com>
2017-08-18 20:31:26 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+class Project
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def backers
+    @backers
+  end
+
+  def add_backer(backer)
+    self.backers << backer
+    backer.backed_projects << self
+  end
+
+
+end
