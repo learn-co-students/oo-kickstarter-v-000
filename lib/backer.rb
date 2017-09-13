117aa69d9be4b97c8cc3bc8aaabdae1647898d0c
Justin Tooley <jstooley823@gmail.com>
2017-09-13 23:26:30 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,13 @@
 class Backer
-  attr_accessor :backed_projects
+  attr_accessor :backed_projects, :name
 
-  def initialize
+  def initialize(name)
     @backed_projects = []
+    @name = name
+  end
+
+  def back_project(name)
+    self.backed_projects << name
+    name.add_backer(self) unless name.backers.include?(self)
   end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,13 @@
 class Project
-  attr_accessor :backers
+  attr_accessor :backers, :title
 
-  def initialize
+  def initialize(title)
     @backers = []
+    @title = title
+  end
+
+  def add_backer(name)
+    self.backers << name
+    name.back_project(self) unless name.backed_projects.include?(self)
   end
 end
