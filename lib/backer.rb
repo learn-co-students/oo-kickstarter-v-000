dadfea23276e787400ef1715334dd903e17b6084
Melissa Rudick <mel.madrugada@gmail.com>
2017-10-06 05:33:00 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,9 +3,10 @@ class Backer
 
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
 
-  def backed_project(project)
+  def back_project(project)
     @backed_projects << project
     project.backers << self
   end
