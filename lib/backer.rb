61064129cb1e7b8cf252a3b7ebeefe350a6e9ad5
Adam Fudge <coolfudge@gmail.com>
2017-10-20 18:48:07 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,14 @@
+class Backer
+  attr_accessor :name, :backed_projects
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    if !(@backed_projects.find {|copy| copy == project})
+      @backed_projects << project
+      project.add_backer(self)
+    end
+  end
+end
