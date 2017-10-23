1f01ddf9b358f1a297f92af597800eacdf36b79d
Julianne Kim <juliannne@gmail.com>
2017-10-23 17:29:33 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,19 +1,13 @@
 class Backer
   attr_accessor :backed_projects, :name
-  attr_reader :backers 
 
   def initialize(name)
     @name = name
     @backed_projects = []
   end
 
-  # def project=(project)
-  #   @project = project
-  #   project.add_backer(self)
-  # end
-
   def back_project(project)
-    @backed_projects << project
+    backed_projects << project
     project.backers << self
   end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,19 +1,13 @@
 class Project
   attr_accessor :backers, :title
-  attr_reader :backed_projects
 
   def initialize(title)
     @title = title
     @backers = []
   end
 
-  # def baker=(baker)
-  #   @baker = baker
-  #   baker.back_project(self)
-  # end
-
   def add_backer(backer)
-    @backers << backer
+    backers << backer
     backer.backed_projects << self
   end
 end
