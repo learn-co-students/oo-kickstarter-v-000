3e75bb1d1cc995ec11d775226772abd22755cb08
Liz Burton <liz.burton147@gmail.com>
2017-12-04 00:53:33 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class Backer
+  attr_reader :name
+  attr_accessor :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    # binding.pry
+    project.add_backer(self) unless project.backers.include?(self)
+  end
+
+end
