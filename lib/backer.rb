255ebacaf9d5e1363d94d9f38977b0642ebe517f
charityyoungblood <youngblood.charity@gmail.com>
2017-12-04 16:08:19 -0800

    added comments on how to approach code

---
+++ lib/backer.rb
@@ -0,0 +1,7 @@
+# create class Backer 
+  # create initialize method w/argument (name) 
+# create class Project 
+  # create initialize method w/argument (name)
+# create back_project method 
+  # w/argument (Project)
+# create add_backer method w/argument (Backer)
