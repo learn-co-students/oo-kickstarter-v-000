22d42078d951a598935c2cf08d172b3fb0747aa9
charityyoungblood <youngblood.charity@gmail.com>
2017-12-04 16:19:38 -0800

    added attr_accessor for name; added @name instance variable to initialize method

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,8 +4,10 @@ require 'pry'
 class Backer 
   # create initialize method w/argument (name)
 # create add_backer method w/argument (Backer)
+  # create backed_projects array - not sure if it's a class or instance variable yet
+  attr_accessor :name
   def initialize(name)
-    
+    @name = name
   end 
   
   
