a3ab2d7e96e5840e6146948f0615595538769a4d
charityyoungblood <youngblood.charity@gmail.com>
2017-12-04 17:07:13 -0800

    added add_backer and back_project method to project.rb and backer.rb

--- lib/project.rb
+++ lib/project.rb
@@ -2,15 +2,16 @@ require 'pry'
 
 # create class Project 
 class Project
-  attr_accessor :title 
-  @@backers = []
+  attr_accessor :title, :backers 
+ 
   # create initialize method w/argument (name)
   def initialize(title)
     @title = title
+    @backers = []
   end
 # # create add_backer method w/argument (Backer)
   # w/argument (Project)
-  def add_backer(backer)
-    @@backers << backer
+  def add_backer(backer_name)
+    @backers << backer_name
   end 
 end
