b462bbf4204dcf2c4b96b6375eef640c978d0e85
Shangni Hu <shangnihu@gmail.com>
2017-12-16 03:59:45 +0000

    Done.

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,7 @@ class Project
   end
 
   def add_backer(backer)
-    @backers.push(backer)
+    @backers << backer
     backer.back_project(self) unless backer.backed_projects.include?(self)
   end
 end
