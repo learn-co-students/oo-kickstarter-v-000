b1fff1636f249bd6527e5ce7d7c7a8cd83b1fc2e
PJ Frias <peejfrias@gmail.com>
2018-01-09 22:16:33 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Backer
+  attr_accessor :backed_projects
+  attr_reader :name
+
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    project.backers << self
+    @backed_projects << project
+    # binding.pry
+  end
+
+end
---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  attr_accessor :backers
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    backer.backed_projects << self
+    @backers << backer
+  end
+
+end
