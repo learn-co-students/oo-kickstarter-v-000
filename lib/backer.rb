eb7140d56784c231557fca2d2e5bd9f7b39e579e
jtorre20 <jtorres1989@gmail.com>
2018-01-27 21:34:34 -0500

    initialized backer and project

---
+++ lib/backer.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+
+class Backer
+  attr_accessor :backed_projects, :name
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+
+  end
+
+end
+
---
+++ lib/project.rb
@@ -0,0 +1,12 @@
+require 'pry'
+
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+
+end
