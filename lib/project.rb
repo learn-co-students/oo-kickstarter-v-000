bbc8a82f59930aa5736e34f428477aef2f40ffc9
Zac Willmington <zac.willmington@gmail.com>
2018-02-24 02:46:15 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,21 @@
+class Project
+
+      attr_accessor :title, :backers
+
+
+
+      def initialize(title)
+            @title = title
+            @backers = []
+      end
+
+      def add_backer(backer)
+             backer.backed_projects << self
+            @backers << backer
+
+
+
+
+      end
+
+end
