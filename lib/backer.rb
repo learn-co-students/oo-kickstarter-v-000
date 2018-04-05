ceb7cdb3bedf23ac10cba17d256fd66b4ef4223e
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-05 00:05:40 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+class Backer
+  attr_accessor :name
+  attr_reader :backed_projects
+
+  def initialize (name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project (project)
+    project.add_backer (self)  # this will also indirectly verify that project is a Project
+    self.add_project(project)
+  end
+
+  def add_project (project)
+      @backed_projects.index(project) || @backed_projects << project
+  end
+end
