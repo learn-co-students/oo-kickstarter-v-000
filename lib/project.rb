2a53ec180b4fea7a8283779d3a84fb93188129c1
Travis Williamson <travywill@gmail.com>
2018-04-20 01:11:14 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Project
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    if backer.backed_projects.include?(self) == false
+      backer.back_project(self)
+    end
+  end
+
+  def backers
+    @backers
+  end
+end
