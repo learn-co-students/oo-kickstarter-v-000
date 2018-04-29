15bc438627a3fed57e7b2e9a10c290e882b7f524
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-29 23:33:30 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+
+class Project
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def backers
+    @backers
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self)
+  end
+
+end
