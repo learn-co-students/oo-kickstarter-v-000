40f53a537adb3d7a21ba65a605777b1701b25dab
Jamie Brown <jmbrowncow@gmail.com>
2018-07-10 15:58:53 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+class Backer
+  attr_accessor :projects
+  attr_reader :name
+
+  def initialize(name)
+    @name=name
+    @projects = []
+  end
+
+  def back_project(project)
+    self.projects<<project
+    project.backers<<self unless project.backers.include?(self)
+  end
+
+  def backed_projects
+    self.projects
+  end
+end
