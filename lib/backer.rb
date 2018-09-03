6fc05b8f44567c11a2603fca10a7338121a53ae3
Jaime Johnsen <drivetraindiva@gmail.com>
2018-09-03 14:27:14 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,4 +5,24 @@ class Backer
   def initialize(name)
     @name = name
     @backed_projects = []
-end
+  end
+
+  def back_project(project)#accepts a Project as an argument and stores it in a backed_projects array
+    @backed_projects << project
+    project.backers << self #adds the backer to the project's backers array.
+
+
+#class Backer
+
+#  attr_accessor :name, :backed_projects
+
+#  def initialize(name)
+#    @name = name
+#    @backed_projects = []
+#  end
+
+#  def back_project(project)
+#    @backed_projects << project
+#    project.backers << self
+#  end
+#end
--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,32 @@
 class Project
 
-attr_accessor :backers#so that projects can be added to a backer's list and so that the backer can report on the projects they back.
+  attr_accessor :title, :backers#so that projects can be added to a backer's list and so that the backer can report on the projects they back.
 
-def initialize
-  @backers = []
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)#accepts a Backer as an argument and stores it in a backers array
+    @backers << backer
+    backer.backed_projects << self#also adds the project to th backer's backed_projects array
+# saIs backed_projects array being called on backer argument then added to the instance of the class project class?
+  end
+
+
+#class Project
+  #attr_accessor :title, :backers
+
+  #def initialize(title)
+  #  @title = title
+  #  @backers = []
+  #end
+
+  #def add_backer(backer)
+  #  @backers << backer
+  #  backer.backed_projects << self
+  #end
+#end
+
+
 end
