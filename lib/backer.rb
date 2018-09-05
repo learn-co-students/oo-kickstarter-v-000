a576740a507890b8f6af1b1e8010356a54eed1b0
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-05 02:48:36 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,3 +1,13 @@
-def Backer
-
+class Backer
+  attr_accessor :name, :backed_projects
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,13 @@
-def Project
+class Project
+  attr_accessor :title, :backers
   
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
 end
