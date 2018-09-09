14ded90248696806de4ff3fd3ed5b336fb1ffe02
Angie Kassab <anasab12@gmail.com>
2018-09-09 16:40:15 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,3 +11,14 @@ class Backer
     project.add_backer(self) unless project.backers.include?(self)
   end
 end
+
+=begin
+
+def back_project(project)       #accepts a Project as an argument and stores it in a backed_projects array
+    @backed_projects << project #telling that Backer instance about on of it's backed projects
+    project.backers << self     #adds the backer to the project's backers array.
+                                #project is an instance of the Project class and calling the backers method on that instance that give you access to the backers array (backers attribute).
+                                #self is an instance of the Backer class, we are storing the backer associated with the project
+end
+
+=end
--- lib/project.rb
+++ lib/project.rb
@@ -11,3 +11,15 @@ class Project
     backer.back_project(self) unless backer.backed_projects.include?(self)
   end
 end
+
+=begin
+
+def add_backer(backer)              #accepts a Backer as an argument and stores it in a backers array
+    @backers << backer
+    backer.backed_projects << self  #also adds the project to th backer's backed_projects array
+                                    #Is backed_projects array being called on backer argument then added to the instance of the class project class?
+end
+                                    #value of self  the type of method it's in, (class or instance). In an instance method it refers to an instance of the class.
+                                    #inside of a class method, self refers to the class itself.
+
+=end
