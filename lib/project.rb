c01ca4b2bd5157d949cdef792d73d8fd2665ce05
miirbat <mirna.plakalovic@gmail.com>
2018-09-29 18:16:16 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(name)
+    @backers << name
+    name.backed_projects << self
+  end
+
+  def self.backers
+    @backers
+  end
+
+end
