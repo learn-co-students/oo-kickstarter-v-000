488e5e0b002086ad5c8e57a2583bc0da59227fc0
Amanda Murphy <helloamandamurphy@gmail.com>
2018-11-06 22:50:15 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,18 +1,18 @@
 require 'pry'
 
 class Backer
-  attr_accessor :name
-  @@backed_projects = []
+  attr_accessor :name, :backed_projects
   
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
   
   def backed_projects
-    @@backed_projects
+    @backed_projects
   end
   
   def back_project(title)
-    @@backed_projects << Project.new(title)
+    @backed_projects << Project.new(title)
   end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,17 +1,17 @@
 class Project
-  attr_accessor :title
-  @@backers = []
+  attr_accessor :title, :backers
   
   def initialize(title)
     @title = title
+    @backers = []
   end
   
   def backers 
-    @@backers
+    @backers
   end 
   
   def add_backer(name)
-    @@backers << Backer.new(name)
-    @@backed_projects << title
+    @backers << Backer.new(name)
+    @backed_projects << title
   end
 end
