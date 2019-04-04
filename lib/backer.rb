0dcb8bf06df84ee113977b62ac04a04aacfea794
Toni-Ty <antonique2479@yahoo.com>
2019-04-04 00:20:38 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,15 @@
+class Backer
+
+attr_accessor
+attr_reader :backed_projects
+
+def initialize
+  @backed_projects = []
+end
+
+def back_project()
+  @backed_projects <<
+
+
+
+end
---
+++ lib/project.rb
@@ -0,0 +1,24 @@
+class Project
+
+attr_accessor
+attr_reader :backers
+
+def initialize
+  @backers = []
+end
+
+def add_backer(backer)
+  @backers << backer
+end
+
+
+
+
+
+
+
+
+
+
+
+end
