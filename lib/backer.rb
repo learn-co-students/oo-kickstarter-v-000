3ce00231f84afe22784eb42d148270f04d46644a
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-05 22:36:04 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+class Backer
+
+  attr_reader :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
+end
+
+def back_project
+  @backers << self
+end
+
+end
